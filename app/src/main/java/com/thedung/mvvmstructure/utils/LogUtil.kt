0bf190cc94158e75ecb4f8b2fa93250f98795854
Dzungvu <dzungvucs@gmail.com>
2020-08-03 12:04:11 +0700

    Add log and modify proguard files

--- app/src/main/java/com/thedung/mvvmstructure/utils/LogUtil.kt
+++ app/src/main/java/com/thedung/mvvmstructure/utils/LogUtil.kt
@@ -1,7 +1,59 @@
 package com.thedung.mvvmstructure.utils
 
-/**
- * Created by VTDung on 8/3/20.
- */
+import android.util.Log
+
+
 class LogUtil {
+
+    private fun getLineNumber(): Int {
+        return try {
+            Thread.currentThread().stackTrace[4].lineNumber
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    fun i(tag: String?, message: String?) {
+        debug {
+            Log.i(tag, String.format("[Line: %d]%s", getLineNumber(), message))
+        }
+
+    }
+
+    fun d(tag: String?, message: String?) {
+        debug {
+            Log.d(tag, String.format("[Line: %d]%s", getLineNumber(), message))
+        }
+    }
+
+    fun e(tag: String?, message: String?) {
+        debug {
+            Log.e(tag, String.format("[Line: %d]%s", getLineNumber(), message))
+        }
+    }
+
+    fun e(tag: String?, message: String?, e: Exception?) {
+        debug {
+            Log.i(tag, String.format("[Line: %d]%s", getLineNumber(), message), e)
+        }
+    }
+
+    fun e(tag: String?, e: Exception) {
+        debug {
+            Log.i(tag, String.format("[Line: %d]%s", getLineNumber(), e.message), e)
+        }
+    }
+
+    fun v(tag: String?, message: String?) {
+        debug {
+            Log.v(tag, String.format("[Line: %d]%s", getLineNumber(), message))
+        }
+    }
+
+    fun w(tag: String?, message: String?) {
+        debug {
+            Log.w(tag, String.format("[Line: %d]%s", getLineNumber(), message))
+        }
+    }
+
 }
